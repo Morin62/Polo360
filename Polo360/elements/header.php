e512b80baa038d7940a766e5b142f4ff5259191c
Morin62 <dbouret@gmail.com>
2016-04-20 11:30:12 +0200

    Update header.php

--- Polo360/elements/header.php
+++ Polo360/elements/header.php
@@ -16,23 +16,7 @@
 					<div class="col-md-4"><img id="logo" src=<?php echo $this->getThemePath(); ?>/img/logo.png alt="logo" /></div>
 					<div class="col-md-8"> <?php $a = new GlobalArea('menu');
 						$a->display($c);?>
-							<ul id="menuhaut" >		<!--le menu horizontal de navigation -->
-								<li><a class="titre" href="#">HOME</a>
-								<p class="sstitre">Back to home</p>
-								</li>
-								<li><a class="titre" href="#">PRODUCT</a>
-								<p class="sstitre">What we have for you</p>
-								</li>
-								<li><a class="titre" href="#">SERVICES</a>
-								<p class="sstitre">Things we do	</p>
-								</li>
-								<li><a class="titre" href="#">BLOG</a>
-								<p class="sstitre">Follow our updates</p>
-								</li>		
-								<li><a class="titre" href="#">CONTACT</a>
-								<p class="sstitre">Ways to reach us</p>		
-								</li>
-							</ul>
+						
 					</div>
 				</div>	<!--fin entete-->
 			</div>		<!--fin class in-->
